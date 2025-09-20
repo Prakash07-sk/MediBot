aad648f5bba8f2500a0d71b1e152faf601fedc12
Vishnu Prakash K <apple@vishnu-prakash-k.local>
2025-09-20 16:00:48 +0530

    first commit

--- Backend/controller/conversation_controller.py
+++ Backend/controller/conversation_controller.py
@@ -1,11 +1,11 @@
 from rag_flow.graphs import GraphFlow
-
+from utils.logger import logger
 class ConversationController:
     def __init__(self):
         self.graph_flow =   GraphFlow()
     async def chat_data(self, payload):
         llm_response = await self.graph_flow.run(payload.query)
-        # Return the response directly without additional wrapping
+        logger.info(f"LLM Response: {llm_response}")
         return llm_response
     
 controller = ConversationController()
